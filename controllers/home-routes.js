298767a0097c7eebb7f676df9f709bd827013849
Daniel Flores D <daniel.flor3s.d@gmail.com>
2022-12-20 21:11:46 -0700

    Handlebar views added for home, login and signup

---
+++ controllers/home-routes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    res.render('homepage', {});
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+    res.render('signup');
+});
+
+module.exports = router;
